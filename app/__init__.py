6797119b9089332c2140ab2369990b2a8650d9c5
Saitore973 <saitorevg@gmail.com>
2022-03-11 23:47:41 +0300

    register the authentication blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -21,8 +21,12 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     # Will add the views and forms
-    #register blueprint
 
+
+    #register blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
     return app
