98310fb17c7f1b11adde7ee6bb36c5380b025d65
Saitore973 <saitorevg@gmail.com>
2022-03-12 00:08:43 +0300

    initialize and instantiate the login manager extension

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,13 @@ from config import config_options
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-
+from flask_login import LoginManager
 
 db=SQLAlchemy()
 bootstrap= Bootstrap()
+login_manager= LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
 
 
 def create_app(config_name):
@@ -20,6 +23,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
     # Will add the views and forms
 
 
--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,4 +1,6 @@
 from flask import Blueprint
-author = Blueprint('auth', __name__)
+
+
+auth = Blueprint('auth',__name__)
 
 from . import views
